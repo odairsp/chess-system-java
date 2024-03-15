69aafc9e09dad32c057813b6501da0d06b11261f
odair <odair.sp@hotmail.com>
2024-03-15 12:09:34 -0300

    Classe Position

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+		
+		Position position = new Position(1, 2);
+		
+		System.out.println(position);
+	}
+
+}
--- src/applicatoin/Program.java
+++
@@ -1,9 +0,0 @@
-package applicatoin;
-
-public class Program {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Word!");
-	}
-
-}
